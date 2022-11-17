aa26678d5c9b62770bbc58af1345652a8746b4f2
Aleleonel <aleleonel@gmail.com>
2022-11-17 18:08:20 -0300

    cadastro de produtos, botão fechar, limpa campos

--- home.py
+++ home.py
@@ -576,104 +576,9 @@ class DeleteClientes(QDialog):
                 QMessageBox(), 'aleleonel@gmail.com', 'A Deleção falhou!')
 
 
-# class CadastroProdutos(QDialog):
-#     """
-#         Define uma nova janela onde cadastramos os clientes
-#     """
-
-#     def __init__(self, *args, **kwargs):
-#         super(CadastroProdutos, self).__init__(*args, **kwargs)
-
-#         self.QBtn = QPushButton()
-#         self.QBtn.setText("Registrar")
-
-#         # Configurações do titulo da Janela
-#         self.setWindowTitle("Add Produto :")
-#         self.setFixedWidth(300)
-#         self.setFixedHeight(300)
-
-#         self.setWindowTitle("Descição do Produto :")
-#         self.setFixedWidth(300)
-#         self.setFixedHeight(300)
-
-#         self.QBtn.clicked.connect(self.addproduto)
-
-#         layout = QVBoxLayout()
-
-#         # Insere o ramo ou tipo /
-#         self.uninput = QComboBox()
-#         self.uninput.addItem("UN")
-#         self.uninput.addItem("PÇ")
-#         self.uninput.addItem("KG")
-#         self.uninput.addItem("LT")
-#         self.uninput.addItem("PT")
-#         self.uninput.addItem("CX")
-#         layout.addWidget(self.uninput)
-
-#         self.descricaoinput = QLineEdit()
-#         self.descricaoinput.setPlaceholderText("Descrição")
-#         layout.addWidget(self.descricaoinput)
-
-#         self.ncminput = QLineEdit()
-#         self.ncminput.setPlaceholderText("NCM")
-#         layout.addWidget(self.ncminput)
-
-#         self.precoinput = QLineEdit()
-#         self.precoinput.setPlaceholderText("Preço.")
-#         layout.addWidget(self.precoinput)
-
-#         layout.addWidget(self.QBtn)
-#         self.setLayout(layout)
-
-#     def addproduto(self):
-#         """
-#         captura as informações digitadas
-#         no lineedit e armazena nas variaveis
-#         :return:
-#         """
-#         descricao = ""
-#         ncm = ""
-#         un = ""
-#         preco = ""
-
-#         descricao = self.descricaoinput.text()
-#         ncm = self.ncminput.text()
-#         un = self.uninput.itemText(self.uninput.currentIndex())
-#         preco = self.precoinput.text()
-
-#         try:
-#             self.cursor = conexao.banco.cursor()
-#             comando_sql = "INSERT INTO produtos (descricao, ncm, un, preco)" \
-#                           "VALUES (%s,%s,%s,%s)"
-#             dados = descricao, ncm, un, str(preco)
-#             self.cursor.execute(comando_sql, dados)
-#             conexao.banco.commit()
-#             self.cursor.close()
-
-#             QMessageBox.information(
-#                 QMessageBox(), 'Cadastro', 'Dados inseridos com sucesso!')
-#             self.close()
-
-#         except Exception:
-
-#             QMessageBox.warning(
-#                 QMessageBox(), 'aleleonel@gmail.com', 'A inserção falhou!')
-
 class CadastroProdutos(QDialog):
     def __init__(self, parent=None):
         super(CadastroProdutos, self).__init__(parent)
-
-        self.originalPalette = QApplication.palette()
-
-        styleComboBox = QComboBox()
-        styleComboBox.addItems(QStyleFactory.keys())
-
-        styleLabel = QLabel("&Style:")
-        styleLabel.setBuddy(styleComboBox)
-
-        self.useStylePaletteCheckBox = QCheckBox(
-            "&Use style's standard palette")
-        self.useStylePaletteCheckBox.setChecked(True)
 
         disableWidgetsCheckBox = QCheckBox("&Disable widgets")
 
@@ -681,19 +586,12 @@ class CadastroProdutos(QDialog):
         self.createGroupBoxSalvar()
         self.createGroupBox()
 
-        styleComboBox.activated[str].connect(self.changeStyle)
-
-        self.useStylePaletteCheckBox.toggled.connect(self.changePalette)
-
         disableWidgetsCheckBox.toggled.connect(self.GroupBox1.setDisabled)
         disableWidgetsCheckBox.toggled.connect(self.GroupBox2.setDisabled)
         disableWidgetsCheckBox.toggled.connect(self.GroupBox3.setDisabled)
 
         topLayout = QHBoxLayout()
-        topLayout.addWidget(styleLabel)
-        topLayout.addWidget(styleComboBox)
         topLayout.addStretch(1)
-        topLayout.addWidget(self.useStylePaletteCheckBox)
         topLayout.addWidget(disableWidgetsCheckBox)
 
         mainLayout = QGridLayout()
@@ -708,22 +606,12 @@ class CadastroProdutos(QDialog):
         self.setLayout(mainLayout)
 
         self.setWindowTitle("CADASTRO DE PRODUTOS")
-        self.changeStyle('Windows')
-
-    def changeStyle(self, styleName):
-        QApplication.setStyle(QStyleFactory.create(styleName))
-        self.changePalette()
-
-    def changePalette(self):
-        if (self.useStylePaletteCheckBox.isChecked()):
-            QApplication.setPalette(QApplication.style().standardPalette())
-        else:
-            QApplication.setPalette(self.originalPalette)
 
     def createTopLeftGroupBox(self):
         self.GroupBox1 = QGroupBox("Cadastro de Itens")
 
-        # Insere o ramo ou tipo /
+        # Insere o ramo ou tipo  /
+        # Criar uma tabela para cadastrar unidade de medida
         self.uninput = QComboBox()
         self.uninput.addItem("UN")
         self.uninput.addItem("PÇ")
@@ -796,11 +684,15 @@ class CadastroProdutos(QDialog):
 
         self.defaultPushButton = QPushButton("Salvar")
         self.defaultPushButton.setDefault(True)
-
         self.defaultPushButton.clicked.connect(self.addproduto)
+
+        self.defaultPushButton2 = QPushButton("Fechar")
+        self.defaultPushButton2.setDefault(True)
+        self.defaultPushButton2.clicked.connect(self.closeCadastro)
 
         layout = QGridLayout()
         layout.addWidget(self.defaultPushButton, 1, 0, 1, 2)
+        layout.addWidget(self.defaultPushButton2, 2, 0, 1, 2)
         layout.setRowStretch(5, 1)
         self.GroupBox3.setLayout(layout)
 
@@ -824,14 +716,20 @@ class CadastroProdutos(QDialog):
             conexao.banco.commit()
             self.cursor.close()
 
-            QMessageBox.information(
-                QMessageBox(), 'Cadastro', 'Dados inseridos com sucesso!')
-            self.close()
+            self.cdprod.setText("")
+            self.descricao.setText("")
+            self.eanprod.setText("")
+            # self.uninput.itemText(self.uninput.currentIndex())
+            self.precocusto.setText("")
+            self.preco.setText("")
 
         except Exception:
 
             QMessageBox.warning(
                 QMessageBox(), 'aleleonel@gmail.com', 'A inserção falhou!')
+
+    def closeCadastro(self):
+        self.close()
 
 
 class ListProdutos(QMainWindow):
@@ -2323,182 +2221,6 @@ class EfetivaPedidoCaixa(QDialog):
             self.errors(e)
 
 
-# class Imprimir(QWidget):
-#     ''' Imprime usando uma box com opções de impressora e visualizações'''
-
-#     def __init__(self, n_caixa):
-#         super(Imprimir, self).__init__()
-#         self.black = "#000000"
-#         self.yellow = "#cfb119"
-#         self.setStyleSheet(
-#             f"background-color: {self.black}; color: {self.yellow};")
-
-#         self.nr_caixa = n_caixa
-
-#         self.vbox = QVBoxLayout()
-#         self.title = "Cadastro"
-#         self.top = 100
-#         self.left = 100
-#         self.width = 640
-#         self.height = 480
-#         self.setWindowIcon(QIcon("Icones/impressora.png"))
-#         self.setLayout(self.vbox)
-
-#         self.cursor = conexao.banco.cursor()
-#         comando_sql = """
-#                         select
-#                             p.codigo,
-#                             p.descricao,
-#                             e.quantidade,
-#                             p.preco,
-#                             e.valor_total
-#                         from
-#                             produtos p
-#                         inner join pedidocaixa e on p.codigo = e.cod_produto
-#                         AND nr_caixa = {0}""".format(str(self.nr_caixa))
-
-#         self.cursor.execute(comando_sql)
-#         dados_lidos = self.cursor.fetchall()
-#         y = 0
-#         pdf = canvas.Canvas("recibo.pdf")
-#         pdf.setFont("Times-Bold", 18)
-#         pdf.drawString(90, 800, "MINA & MINEKO ART. FAMELE:")
-#         pdf.setFont("Times-Bold", 12)
-
-#         pdf.drawString(10, 750, "ID")
-#         pdf.drawString(50, 750, "PRODUTO")
-#         pdf.drawString(260, 750, "QTD.")
-#         pdf.drawString(310, 750, "PREÇO UN.")
-#         pdf.drawString(390, 750, "SUB.TOTAL")
-#         pdf.drawString(470, 750, "TOTAL")
-#         pdf.drawString(3, 750,
-#                        "________________________________________________________________________________________")
-
-#         total = 0
-#         subtotal = 0
-#         for i in range(0, len(dados_lidos)):
-#             y += 50
-#             # CODIGO PRODUTO
-#             pdf.drawString(10, 750 - y, str(dados_lidos[i][0]))
-#             # DESCRIÇAO PRODUTO
-#             pdf.drawString(50, 750 - y, str(dados_lidos[i][1]))
-#             # QUANTIDADE VENDIDA
-#             pdf.drawString(260, 750 - y, str(dados_lidos[i][2]))
-#             # PREÇO UNITARIO
-#             pdf.drawString(310, 750 - y, str(dados_lidos[i][3]))
-#             subtotal = (dados_lidos[i][3]) * \
-#                 dados_lidos[i][2]  # QTD x PREÇO UNITARIO
-#             total += subtotal
-#             pdf.drawString(390, 750 - y, str(subtotal))  # SUB TOTAL
-#         pdf.drawString(470, 750 - y, str(total))  # TOTAL
-
-#         pdf.save()
-
-#         with open('recibo.csv', 'w') as f:
-#             csv_writer = csv.writer(f)
-#             rows = [i for i in dados_lidos]
-#             csv_writer.writerows(rows)
-
-#         self.InitWindow()
-
-#     def InitWindow(self):
-#         '''Permie visualizar a impressão em tela, mas estva fechando a tela principal
-#         por isso eu desativei a chamada'''
-#         self.hbox = QHBoxLayout()
-
-#         self.print_btn = QPushButton("IMPRIMIR", self)
-#         self.print_btn.clicked.connect(self.print)
-
-#         self.view_btn = QPushButton("VISUALIZAR", self)
-#         self.view_btn.clicked.connect(self.view)
-
-#         self.rw = PrettyTable()
-#         self.rw.field_names = ["Cod.", "Descrição",
-#                                "Quant.", "Preco unit.", "Subtotal\n"]
-#         try:
-#             with open("recibo.csv", "r") as msg:
-#                 self.lin = [x.strip().split(",") for x in msg]
-
-#                 self.a = [self.lin[x] for x in range(len(self.lin))]
-
-#                 total = 0
-#                 for x in self.a:
-#                     self.rw.set_style(PLAIN_COLUMNS)
-#                     self.rw.add_row(x)
-
-#                 for sub in range(len(self.a)):
-#                     total += float(self.a[sub][4])
-
-#                 print(self.rw)
-#                 msg.close()
-#         except Exception as e:
-#             self.errors(e)
-
-#         empresa = "MINA & MINEKO ART. FEMININS E TABACARIA"
-#         endereco = "Rua Enestina Loschi n.76"
-#         telefone = "(11) 97151-2237 / (11) 97561-8992)"
-#         text = f'\n\nTOTAL: {total:>130} \n\nEmpresa: {empresa:^90} \nEndereço: {endereco:^90} \nTelefones: {telefone:^90}'
-
-#         newGroup = QGroupBox("LISTA DE PRODUTOS")
-#         newGroup.setStyleSheet(
-#             f"background-color: {self.black}; color: {self.yellow}")
-
-#         self.edt = QTextEdit(self)
-#         self.edt.setAcceptRichText(True)
-#         self.edt.setReadOnly(True)
-#         self.edt.setText(f"{self.rw} \n{text}")
-#         self.vbox.addWidget(self.edt)
-
-#         self.hbox.addWidget(self.print_btn)
-#         # self.hbox.addWidget(self.view_btn)
-#         self.vbox.addLayout(self.hbox)
-
-#         self.setWindowTitle(self.title)
-#         self.setGeometry(self.top, self.left, self.width, self.height)
-#         self.show()
-
-#     def errors(self, e):
-#         print(e)
-#         exc_type, exc_obj, exc_tb = sys.exc_info()
-#         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-#         err = f"{e} \n{exc_type} \n{fname} \n{exc_tb.tb_lineno} \n"
-#         print(err)
-
-#     def print(self):
-#         prt = QPrinter(QPrinter.HighResolution)
-#         dialog = QPrintDialog(prt)
-
-#         if dialog.exec_() == QPrintDialog.Accepted:
-#             self.edt.print_(prt)
-#             # self.hide()
-#             telaprincipal()
-#         # telaprincipal()
-
-#     def view(self):
-#         pt = QPrinter(QPrinter.HighResolution)
-#         prev = QPrintPreviewDialog(pt, self)
-#         prev.paintRequested.connect(self.preview)
-#         prev.exec_()
-
-#     def preview(self, pt):
-#         self.edt.print_(pt)
-
-#     # testar a criação de um arquivo csv baseado na table
-#     def export_to_csv(self, w):
-#         try:
-#             with open('recibo.csv', 'w', newline='') as file:
-#                 writer = csv.writer(file)
-#                 writer.writerow((w.table.horizontalHeaderItem(
-#                     0).text(), w.table.horizontalHeaderItem(1).text()))
-#                 for rowNumber in range(w.table.rowCount()):
-#                     writer.writerow(
-#                         [w.table.item(rowNumber, 0).text(), w.table.item(rowNumber, 1).text()])
-#                 print('CSV file exported.')
-#             file.close()
-#         except Exception as e:
-#             print(e)
-
-
 class ListPedidos(QMainWindow):
     def __init__(self):
         super(ListPedidos, self).__init__()
@@ -2828,11 +2550,6 @@ class MainWindow(QMainWindow):
         btn_ac_estoque.setStatusTip("Estoque")
         toolbar.addAction(btn_ac_estoque)
 
-        # btn_ac_caixa = QAction(QIcon("Icones/dollars.png"), "Caixa - abre o Caixa", self)
-        # btn_ac_caixa.triggered.connect(self.caixa)
-        # btn_ac_caixa.setStatusTip("Caixa")
-        # toolbar.addAction(btn_ac_caixa)
-
         btn_ac_pedido = QAction(
             QIcon("Icones/produtos.png"), "Listar Pedidos", self)
         btn_ac_pedido.triggered.connect(self.listPedido)
@@ -2865,10 +2582,6 @@ class MainWindow(QMainWindow):
         btn_ac_estoque.triggered.connect(self.listEstoque)
         file_menu.addAction(btn_ac_estoque)
 
-        # btn_ac_caixa = QAction(QIcon("Icones/dollars.png"), "Caixa", self)
-        # btn_ac_caixa.triggered.connect(self.caixa)
-        # file_menu.addAction(btn_ac_caixa)
-
         btn_ac_Cupon = QAction(
             QIcon("Icones/impressora.png"), "Imprimir Cupon", self)
         btn_ac_Cupon.triggered.connect(self.cupon)
@@ -2890,8 +2603,8 @@ class MainWindow(QMainWindow):
         export_Action.triggered.connect(self.export_to_csv)
         file_menu.addAction(export_Action)
 
-        self.show()
-        # self.showFullScreen()
+        # self.show()
+        self.showFullScreen()
 
     def about(self):
         dlg = AboutDialog()
