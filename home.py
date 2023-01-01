6b0d2c30d0afa7eccb97879f89d99dcaa6c92cc6
Aleleonel <aleleonel@gmail.com>
2023-01-01 16:34:00 -0300

    corrigindo relatório de fechamento de caixa para multiplos fechamentos diarios

--- home.py
+++ home.py
@@ -2812,15 +2812,16 @@ class MainWindow(QMainWindow):
         dlg.exec()
 
     def relatorioFechamentocaixa(self):
+        """ Relatório de fechamento do Caixa"""
 
         dataAtual = QDate.currentDate()
         data = dataAtual.toPyDate()
 
-        print("Convertendo esta data", data)
-
         valor_do_dia = []
+        vendas_do_dia = []
         self.soma_fechamento = 0
         valor_inicial = 0
+        valor_abertura = []
 
         self.cursor = conexao.banco.cursor()
         consulta_sql = "SELECT valor FROM livro where status = '{}' and dataAtual = '{}';".format(
@@ -2829,8 +2830,10 @@ class MainWindow(QMainWindow):
         self.valor_inicial = self.cursor.fetchall()
 
         for val in range(len(self.valor_inicial)):
-            print("\nTEM ALGUMA COISA AQUI", self.valor_inicial[val][0])
-            valor_inicial = self.valor_inicial[val][0]
+            valor_inicial = float(self.valor_inicial[val][0])
+            valor_abertura.append(valor_inicial)
+
+        print("Mostra valores que iniciamos o caixa", valor_abertura)
 
         try:
             self.cursor = conexao.banco.cursor()
@@ -2842,7 +2845,7 @@ class MainWindow(QMainWindow):
             print("Sem valor de fechamento", e)
 
         for val in range(len(self.valor_fechamento)):
-            valor_fechamento = self.valor_fechamento[val][0]
+            valor_fechamento = float(self.valor_fechamento[val][0])
 
         try:
             self.cursor = conexao.banco.cursor()
@@ -2866,9 +2869,10 @@ class MainWindow(QMainWindow):
             # DATA DO FECHAMENTO
             pdf.drawString(
                 10, 700 - y, str('{}').format(data_fechamento[dtnumber][0]))
+
             # VALOR DA ABERTURA
-            pdf.drawString(
-                90, 700 - y, str({}).format(valor_inicial))
+
+            # pdf.drawString(90, 700 - y, str({}).format(valor_inicial))
 
             pdf.drawString(10, 750, "Data")
             pdf.drawString(90, 750, "Abertura")
@@ -2885,23 +2889,34 @@ class MainWindow(QMainWindow):
             cont = len(i)
             c = 0
             while cont > 0:
-                total += i[c]
+                total += float(i[c])
                 cont -= 1
                 c += 1
 
+            total_abertura = 0
+            i_valorAbertura = [i for i in valor_abertura]
+
+            contador = len(i_valorAbertura)
+            ct = 0
+            print("Valor tota dae todas as aberturas do dia ", total_abertura)
+
+            while contador > 0:
+                caixa_iniciado = i_valorAbertura[ct]
+                total_abertura += i_valorAbertura[ct]
+                pdf.drawString(90, 700 - y, str({}).format(caixa_iniciado))
+                contador -= 1
+                ct += 1
+                y += 50
+
             subtotal = (float(total) + float(self.valor_inicial[val][0]))
             y += 50
+
+            pdf.drawString(90, 750 - y, str({}).format(total_abertura))
             pdf.drawString(160, 750 - y, str(total))
-            pdf.drawString(220, 750 - y, str(valor_fechamento))  # TOTAL
-
-            # pdf.drawString(
-            # 290, 750 - y, str(self.soma_fechamento))  # TOTAL
-
+            pdf.drawString(220, 750 - y, str(valor_fechamento))
+            pdf.drawString(310, 750 - y, str(subtotal))
             pdf.drawString(
-                310, 750 - y, str(subtotal))  # TOTAL
-
-            pdf.drawString(
-                440, 750 - y, str(subtotal - valor_fechamento))
+                440, 750 - y, str(float(valor_fechamento) - float(subtotal)))
 
             pdf.save()
 
